a7f47e6712ea6195f3abc5daadaeb2747970c683
pramitbhar26 <pramitbhar26@gmail.com>
2024-12-25 08:14:45 +0530

    Add Frontend Components and Sign in page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Route,Routes,BrowserRouter} from 'react-router-dom'
+import { Signin } from './pages/Signin'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        Hello World
-        </div>
+      <BrowserRouter>
+       <Routes>
+        <Route path='/signin' element={<Signin/>} />
+        <Route path='/signup' element={<signup />} />
+
+        <Route path='/signup' element={<signup />} />
+
+        <Route path='/signup' element={<signup />} />
+
+       </Routes>
+      </BrowserRouter>
     </>
   )
 }
